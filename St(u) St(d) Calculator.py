a4f0fa8d9adc272f2de33ad8bcaaead2a99788ae
Starfih <150697957+Starfih@users.noreply.github.com>
2026-01-12 12:12:30 -0500

    Variables

---
+++ St(u) St(d) Calculator.py
@@ -0,0 +1,23 @@
+#COX ROSS RUBENSTEIN STOCK PRICE CALCULATOR
+#This script is to calculate the stock price tree given parameters to be used in the calculations of portfolio delta
+#Jaydon Thinh-To Jan 12 2026
+
+
+import numpy as np
+import math as math
+
+
+Time = 0 #Time | T
+Strike = 0 #Strike Price | K
+interest_rate = 0.0 #Interest Rate | r
+volatility = 0 #Sigma (Volatility) |  vol
+div_yield = 0 #Dividend Yield | q
+height = 0 #Height of Tree | n
+
+def crr_calculation(T,K,r, vol, q, n):
+    print(T, K, r, vol, q, n)
+
+
+
+
+crr_calculation(Time, Strike, interest_rate, volatility, div_yield, height)
